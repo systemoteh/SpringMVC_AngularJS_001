fc870131f6c7bff73e7bed16b2a816964003f4f6
systemoteh <aleksei_0888@mail.ru>
2018-06-26 21:47:07 +0300

    Added fileUploader, Excel and PDF Exporter

---
+++ src/main/java/ru/systemoteh/spring_angular_001/mvc/excelpdf/Cat.java
@@ -0,0 +1,64 @@
+package ru.systemoteh.spring_angular_001.mvc.excelpdf;
+
+import java.util.Objects;
+
+public class Cat {
+
+    private String name;
+    private String color;
+    private int weight;
+
+    public Cat(String name, String color, int weight) {
+        this.name = name;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return weight == cat.weight &&
+                Objects.equals(name, cat.name) &&
+                Objects.equals(color, cat.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, color, weight);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
